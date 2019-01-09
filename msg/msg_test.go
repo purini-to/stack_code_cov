70548c388012b6c1b3984f3c27ee27c0b4f2b351
hiroshi urainkyo <biz.hr.urainkyo@gmail.com>
2019-01-09 18:35:48 +0900

    fix typo

--- msg/msg_test.go
+++ msg/msg_test.go
@@ -7,7 +7,7 @@ func TestGetMessage(t *testing.T) {
 		name string
 		want string
 	}{
-		{name: "echo文字列を返却すること", want: "Go World."},
+		{name: "echo文字列を返却すること", want: "Go We are the world."},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
